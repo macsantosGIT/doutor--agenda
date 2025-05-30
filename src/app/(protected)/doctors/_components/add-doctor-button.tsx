1b70be7777a7d123a5fcb7e894af923cb6ea7808
macsantosGIT <marcelo.correa.santos@gmail.com>
2025-05-30 12:46:12 -0300

    feat: add doctor creation form

---
+++ src/app/(protected)/doctors/_components/add-doctor-button.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { DialogTrigger } from "@/components/ui/dialog";
+import { Dialog } from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
+import UpsertDoctorForm from "./upsert-doctor-form";
+
+const AddDoctorButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus />
+          Adicionar médico
+        </Button>
+      </DialogTrigger>
+      <UpsertDoctorForm />
+    </Dialog>
+  );
+};
+
+export default AddDoctorButton;
